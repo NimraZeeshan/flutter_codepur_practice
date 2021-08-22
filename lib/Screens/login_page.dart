5ccb2f5deab8e5027103b244c1c315f9fa8116ee
NimraZeeshan <nimraghazal19@gmail.com>
2021-08-23 00:29:01 +0500

    Made Login Screen and Added Field

--- lib/Screens/login_page.dart
+++ lib/Screens/login_page.dart
@@ -6,13 +6,61 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          'login Page',
-          style: TextStyle(
-              fontSize: 25, color: Colors.cyan, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset("assets/images/Login_image.png",
+                height: 160, fit: BoxFit.cover),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Welcome',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.blue[700],
+                  fontWeight: FontWeight.w900),
+            ),
+            Text(
+              "Water Level Monitoring & Control",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter username',
+                      labelText: 'UserName',
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Enter password',
+                      labelText: 'Password',
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Hi Nimra");
+                    },
+                    child: Text('Login'),
+                    style: TextButton.styleFrom(),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codepur_practice/Screens/home_page.dart';
 import 'package:flutter_codepur_practice/Screens/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,8 +13,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: Homepage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.cyan),
-      initialRoute: "/home",
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => Homepage(),
